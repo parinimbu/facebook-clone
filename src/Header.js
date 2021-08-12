92a3a5431095c321a89cd3240dc4f1a1afe21b9a
parinimbu <limbooparinn@gmail.com>
2021-08-12 23:08:26 +0545

    Search Bar

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import './Header.css'
+import React from 'react';
+import './Header.css';
+import SearchIcon from "@material-ui/icons/Search"
 
 function Header() {
     return (
         <div className="header">
-            <div className='header__left'></div>
+            <div className='header__left'>
             <img
               src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/51/Facebook_f_logo_%282019%29.svg/1200px-Facebook_f_logo_%282019%29.svg.png" alt=""
             />  
             
+            <div className="header__input">
+                <SearchIcon />
+
+                <input type=""/>
+
+            </div>
+            
+            </div>
+
             <div className='header__middle'></div>
 
             <div className='header__middle'></div>
