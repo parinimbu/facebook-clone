39bfd223bfa8664e2a40a15479ad666190691fdd
parinimbu <limbooparinn@gmail.com>
2021-08-15 10:34:54 +0545

    Story images

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,10 +1,40 @@
 import React from 'react';
+import Story from './Story';
 import './StoryReel.css'
 
 function StoryReel() {
     return (
         <div className='storyReel'>
-            
+            { /* Story */}
+            <Story
+            image="https://photos.google.com/photo/AF1QipODMkyCCq4iYlEvLZtXVrXl0YeR_dVnleKlRB_P"
+            profileSrc="https://photos.google.com/photo/AF1QipOv2m5l5Fp57oOy96r19wsaR3B_NVhHTJWdjVpY"
+            title="Parin Limbu"
+            />
+
+            <Story
+            image="https://photos.google.com/photo/AF1QipODMkyCCq4iYlEvLZtXVrXl0YeR_dVnleKlRB_P"
+            profileSrc="https://photos.google.com/photo/AF1QipOv2m5l5Fp57oOy96r19wsaR3B_NVhHTJWdjVpY"
+            title="Parin Limbu"
+            />
+
+            <Story
+            image="https://photos.google.com/photo/AF1QipODMkyCCq4iYlEvLZtXVrXl0YeR_dVnleKlRB_P"
+            profileSrc="https://photos.google.com/photo/AF1QipOv2m5l5Fp57oOy96r19wsaR3B_NVhHTJWdjVpY"
+            title="Parin Limbu"
+            />
+
+            <Story
+            image="https://photos.google.com/photo/AF1QipODMkyCCq4iYlEvLZtXVrXl0YeR_dVnleKlRB_P"
+            profileSrc="https://photos.google.com/photo/AF1QipOv2m5l5Fp57oOy96r19wsaR3B_NVhHTJWdjVpY"
+            title="Parin Limbu"
+            />
+
+            <Story
+            image="https://photos.google.com/photo/AF1QipODMkyCCq4iYlEvLZtXVrXl0YeR_dVnleKlRB_P"
+            profileSrc="https://photos.google.com/photo/AF1QipOv2m5l5Fp57oOy96r19wsaR3B_NVhHTJWdjVpY"
+            title="Parin Limbu"
+            />
         </div>
     )
 }
