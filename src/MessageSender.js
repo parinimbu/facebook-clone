9edfb94219eedb1e8f62f6ca87d62315c7665c5e
parinimbu <limbooparinn@gmail.com>
2021-08-15 22:09:25 +0545

    MessageInput feilds created

---
+++ src/MessageSender.js
@@ -0,0 +1,42 @@
+import { Avatar } from '@material-ui/core';
+import React from 'react';
+import './MessageSender.css';
+import VideocamIcon from "@material-ui/icons/Videocam";
+import PhotoLibraryIcon from '@material-ui/icons/PhotoLibrary';
+import InsertEmoticonIcon from '@material-ui/icons/InsertEmoticon';
+
+function MessageSender() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+    }
+    return (
+        <div className="messageSender">
+            <div className="messageSender__top">
+                <Avatar />
+                <form>
+                    <input
+                        className="messageSender__input"
+                        placeholder={"What's on your mind"}
+                    />
+                    <input
+                        placeholder="image URL (Optional)" 
+                    />
+
+                    <button onClick={handleSubmit} type="submit">
+                        Hidden submit
+                    </button>
+                </form>
+            </div>
+
+            <div className="messageSender__bottom">
+                <div className="messageSender__option">
+                    <VideocamIcon style={{color: "red" }}/>
+                    <h3>Live Video</h3>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default MessageSender
