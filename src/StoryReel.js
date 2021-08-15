b52cf5a3a428aaab420619fe97686827938b3b0e
parinimbu <limbooparinn@gmail.com>
2021-08-15 20:56:51 +0545

    Story css

--- src/StoryReel.js
+++ src/StoryReel.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import Story from './Story';
-import './StoryReel.css'
+import './StoryReel.css';
 
 function StoryReel() {
     return (
         <div className='storyReel'>
             { /* Story */}
             <Story
-            image="https://photos.google.com/photo/AF1QipODMkyCCq4iYlEvLZtXVrXl0YeR_dVnleKlRB_P"
+            image="https://mk0laterblogouwugirk.kinstacdn.com/wp-content/uploads/2019/01/How-Often-to-Post-to-Facebook-Stories.png"
             profileSrc="https://photos.google.com/photo/AF1QipOv2m5l5Fp57oOy96r19wsaR3B_NVhHTJWdjVpY"
             title="Parin Limbu"
             />
