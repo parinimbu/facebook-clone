fa8b305c7d61b89b5e6a8f3f363287e3edd5ff52
parinimbu <limbooparinn@gmail.com>
2021-08-19 19:09:03 +0545

    iframe widgirs integerted

--- src/Widgets.js
+++ src/Widgets.js
@@ -4,7 +4,16 @@ import './Widgets.css';
 function Widgets() {
     return (
         <div className="widgets">
-            
+            <iframe
+            src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2fwww.facebook.com%2FCleverProgrammerr%2F&tabs=timeline&width=340&height=1500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appID"
+            width="340"
+            height="100%"
+            style={{ border: "none", overflow: "hidden"}}
+            scrolling="no"
+            frameBorder="0"
+            allowTransparency="true"
+            allow="encrypted-media"
+            ></iframe>
         </div>
     )
 }
