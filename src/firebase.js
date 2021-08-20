867efa38ac5b358b130e8210b283dcd34262f00a
parinimbu <limbooparinn@gmail.com>
2021-08-20 23:53:21 +0545

    firebase db and auth initialized

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyC_2NFMc1fSBLsJN6yK9kYW1FJwJl-a4dM",
@@ -8,3 +10,7 @@ const firebaseConfig = {
     appId: "1:481952358770:web:6022c9b4ae067ccdd9fdeb",
     measurementId: "G-GT48JBFMGY"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
