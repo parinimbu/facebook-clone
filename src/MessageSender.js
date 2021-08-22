594b906acaafbf7880c98752f18871d71929891d
parinimbu <limbooparinn@gmail.com>
2021-08-22 22:08:36 +0545

    firestore initialization

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -28,7 +28,7 @@ function MessageSender() {
                         value={input}
                         onChange={(e) => setInput(e.target.value)}
                         className="messageSender__input"
-                        placeholder={"What's on your mind"}
+                        placeholder={`What's on your mind, ${user.displayName}`}
                     />
                     <input
                         value={imageUrl}
